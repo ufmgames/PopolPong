e75cf253d43a452ac80ae60f98225354ea24f1fb
Yakaffaty <41757185+Yakaffaty@users.noreply.github.com>
2018-09-03 15:18:14 -0600

    CompleteClone

---
+++ PopolPong/Assets/Scripts/DeadZone.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+
+
+    void OnTriggerEnter(Collider col)
+    {
+        GM.instance.LoseLife();
+    }
+}
