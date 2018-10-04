4515afbab1378f9ecd05618bf294cf4929c4d4c7
byron <byronap120@gmail.com>
2018-10-04 02:17:06 -0600

    Ring
    - detect goal on empty ring

---
+++ PopolPong2D/Assets/Scripts/Ball.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    private bool isGoal = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Ring")
+        {
+            Debug.Log("Enter");
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Ring")
+        {
+            Debug.Log("Stay");
+            if (!isGoal)
+            {
+                Ring goalRing = collision.gameObject.GetComponent<Ring>();
+                if(goalRing.CheckGoal(this.gameObject))
+                {
+                    Debug.Log("Goalllllllllllllllll");
+                    isGoal = true;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Ring")
+        {
+            Debug.Log("Exit");
+            isGoal = false;
+        }
+    }
+}
